096051ea7582f3f3e2ad9265f23fa31afc0b37cf
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2022-02-01 10:43:11 -0500

    Seprando archivo con data de la pagina principal

--- src/App.js
+++ src/App.js
@@ -2,31 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Film from './Film';
 import PageWrapper from './PageWrapper';
-
+import {films} from './FilmsData'
+ 
 function App() {
 
-  let films = [
-    {
-      img: "images/uploads/mv1.jpg",
-      title: "Oblivion (2012)" ,
-      rating: "8.1",
-      director: "Joss Whedon",
-      actors: "Robert Downey Jr.,Chris Evans, Chris Hemsworth",
-      date: "1 May 2015",
-      duration: "2h 21min",
-      description: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-      img: "images/uploads/mv2.jpg",
-      title: "into the wild (2014)", 
-      rating: "7.8",
-      director: "Anthony Russo, Joe Russo",
-      actors: "Chris Evans, Samuel L. Jackson, Scarlett Johansson",
-      date: "1 May 2015",
-      duration: "2h 21min",
-      description: "As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat..."
-    }
-  ];
+  
 
   return (
     <PageWrapper>
---
+++ src/FilmsData.js
@@ -0,0 +1,152 @@
+export let films = [
+  {
+    img: "images/uploads/mv1.jpg",
+    title: "Oblivion (2012)",
+    rating: "8.1",
+    director: "Joss Whedon",
+    actors: "Robert Downey Jr.,Chris Evans, Chris Hemsworth",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+  },
+  {
+    img: "images/uploads/mv2.jpg",
+    title: "into the wild (2014)",
+    rating: "7.8",
+    director: "Anthony Russo, Joe Russo",
+    actors: "Chris Evans, Samuel L. Jackson, Scarlett Johansson",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "As Steve Rogers struggles to embrace his role in the modern world, he teams up with a fellow Avenger and S.H.I.E.L.D agent, Black Widow, to battle a new threat..."
+  },
+  {
+    img: "images/uploads/mv3.jpg",
+    title: "BLADE RUNNER (2015)",
+    rating: "7.3",
+    director: "Peyton Reed",
+    actors: "Paul Rudd, Michael Douglas",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
+  },
+  {
+    img: "images/uploads/mv4.jpg",
+    title: "MULHOLLAND PRIDE (2013)",
+    rating: "7.2",
+    director: "Shane Black",
+    actors: "Robert Downey Jr., Guy Pearce,Don Cheadle",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+  },
+  {
+    img: "images/uploads/mv5.jpg",
+    title: "SKYFALL: EVIL OF BOSS (2013)",
+    rating: "7.0",
+    director: "Alan Taylor",
+    actors: "Chris Hemsworth, Natalie Portman,Tom Hiddleston",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+  },
+  {
+    img: "images/uploads/mv1.jpg",
+    title: "oblivion (2012)",
+    rating: "8.1",
+    director: "Joss Whedon",
+    actors: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+  },
+  {
+    img: "images/uploads/mv2.jpg",
+    title: "into the wild (2014)",
+    rating: "7.5",
+    director: "Joss Whedon",
+    actors: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+  },
+  {
+    img: "images/uploads/mv3.jpg",
+    title: "BLADE RUNNER (2015)",
+    rating: "7.3",
+    director: "Peyton Reed",
+    actors: "Paul Rudd, Michael Douglas",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
+  },
+  {
+    img: "images/uploads/mv4.jpg",
+    title: "MULHOLLAND PRIDE (2013)",
+    rating: "7.2",
+    director: "Shane Black",
+    actors: "Robert Downey Jr., Guy Pearce,Don Cheadle",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+  },
+  {
+    img: "images/uploads/mv5.jpg",
+    title: "SKYFALL: EVIL OF BOSS (2013)",
+    rating: "7.0",
+    director: "Alan Taylor",
+    actors: "Chris Hemsworth, Natalie Portman,Tom Hiddleston",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+  },
+  {
+    img: "images/uploads/mv1.jpg",
+    title: "oblivion (2012)",
+    rating: "8.1",
+    director: "Joss Whedon",
+    actors: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+  },
+  {
+    img: "images/uploads/mv2.jpg",
+    title: "into the wild (2014)",
+    rating: "7.5",
+    director: "Joss Whedon",
+    actors: "Robert Downey Jr., Chris Evans, Chris Hemsworth",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+  },
+  {
+    img: "images/uploads/mv3.jpg",
+    title: "BLADE RUNNER (2015)",
+    rating: "7.3",
+    director: "Peyton Reed",
+    actors: "Paul Rudd, Michael Douglas",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
+  },
+  {
+    img: "images/uploads/mv4.jpg",
+    title: "MULHOLLAND PRIDE (2013)",
+    rating: "7.2",
+    director: "Shane Black",
+    actors: "Robert Downey Jr., Guy Pearce,Don Cheadle",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+  },
+  {
+    img: "images/uploads/mv5.jpg",
+    title: "SKYFALL: EVIL OF BOSS (2013)",
+    rating: "7.0",
+    director: "Alan Taylor",
+    actors: "Chris Hemsworth, Natalie Portman,Tom Hiddleston",
+    date: "1 May 2015",
+    duration: "2h 21min",
+    description: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+  }
+];
