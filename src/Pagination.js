6a2acd9faf8dec138b0af436999d445af9fba8e9
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2022-02-03 23:46:59 -0500

    Generando logica de la paginacion desde el componente

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,12 +1,24 @@
 function Pagination(props) {
 
+  const getPages = () => {
+    const result = []
+    for (let i = 0; i < props.total; i ++) {
+      let page = i + 1;
+      result.push(
+        <a onClick={() => props.onChange(page)}
+          className={props.page === page ? 'active' : ''} >
+          {page}
+        </a>
+      )
+    }
+    return result;
+  }
+
   return (
     <div className="topbar-filter">
       <div className="pagination2">
         <span>Page {props.page} of {props.total}:</span>
-        <a className="active" href="#">1</a>
-        <a href="#">2</a>
-        <a href="#"><i className="ion-arrow-right-b"></i></a>
+        {getPages()}
       </div>
     </div>
   )
